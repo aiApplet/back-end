15d8674708835080480947a55ee0c5b27882c2c7
anning <864399407@qq.com>
2024-01-01 02:11:20 +0800

    设置静态路径

--- back_end/settings.py
+++ back_end/settings.py
@@ -142,7 +142,8 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, STATIC_URL)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
