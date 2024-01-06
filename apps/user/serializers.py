6ad7591d6c4c1512b65ac85a014c6f9a58a74aca
anning <864399407@qq.com>
2024-01-06 13:23:47 +0800

    no message

--- apps/user/serializers.py
+++ apps/user/serializers.py
@@ -15,7 +15,7 @@ class UserSerializer(ModelSerializer):
 
     class Meta:
         model = User
-        fields = ['id', 'is_nickname', 'avatar', 'balance', 'is_avatar']
+        fields = ['id', 'is_nickname', 'avatar', 'balance']
         extra_kwargs = {
             'balance': {'read_only': True},
         }
